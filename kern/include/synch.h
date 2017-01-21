9828149da6198c9cf574cc7fbe6540325d672a91
odzick <odzick@gmail.com>
2017-01-21 12:54:46 -0800

    implement locks and condition variables

--- kern/include/synch.h
+++ kern/include/synch.h
@@ -74,8 +74,10 @@ void V(struct semaphore *);
  */
 struct lock {
         char *lk_name;
-        // add what you need here
-        // (don't forget to mark things volatile as needed)
+        volatile bool lk_is_held;
+        volatile struct thread *lk_owner;
+        struct wchan *lk_wchan;
+        struct spinlock lk_lock;
 };
 
 struct lock *lock_create(const char *name);
@@ -113,8 +115,8 @@ bool lock_do_i_hold(struct lock *);
 
 struct cv {
         char *cv_name;
-        // add what you need here
-        // (don't forget to mark things volatile as needed)
+        struct wchan *cv_wchan;
+        struct spinlock cv_lock;
 };
 
 struct cv *cv_create(const char *name);
