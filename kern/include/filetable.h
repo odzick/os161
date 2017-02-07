24c925a8c3d1512e6461acb3c3f1f9ac74351211
odzick <odzick@gmail.com>
2017-02-06 19:03:36 -0800

    add file table functions

--- kern/include/filetable.h
+++ kern/include/filetable.h
@@ -2,6 +2,9 @@
 #ifndef _FILETABLE_H_ 
 #define _FILETABLE_H_
 
+#include<vnode.h>
+#include<type.h>
+
 #define MAX_FILES 30
 
 struct file{
@@ -14,8 +17,11 @@ struct file{
 struct filetable{
         struct lock *ft_lock;
         struct file *files[MAX_FILES];
+        int last;
 };
 
 filetable* ft_create(void);
+void ft_destroy(filetable*);
+void ft_add(filetable* ft, int fd, file* file) 
 
 #endif /* _FILETABLE_H_ */
---
+++ kern/vfs/filetable.c
@@ -0,0 +1,45 @@
+#include<filetable.h>
+#include<vnode.h>
+#include<type.h>
+
+
+
+filetable* 
+ft_create(void)
+{
+    int i;
+
+    struct filetable* ft = kmalloc(sizeof(struct filetable));
+
+    for(i = 0; i < MAX_FILES; i++)
+        ft->files[i] = NULL;
+
+    ft->last = 0;
+    ft->lock = lock_create("file_table lock");
+
+    return ft; 
+}
+
+void
+ft_destroy(filetable* ft)
+{
+    lock_destroy(ft->lock);
+    kfree(ft);
+}
+
+//TODO: check every index for last
+void
+ft_add(filetable* ft, int fd, file* file) 
+{
+   int i; 
+
+   if(ft->files[ft->last] != NULL) 
+       return;
+
+    ft->files[ft->last] = file; 
+}
+
+void
+ft_remove()
+{
+}
